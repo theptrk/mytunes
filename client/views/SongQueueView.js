fc0f4be3d7204abbf9065fac00cbc358e9cdeabe
Hack Reactor Students <patrick.tran06@gmail.com>
2014-04-30 20:05:43 -0700

    SongQueueView logic that auto-updates the templates

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,11 +1,24 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: "table",
+
   initialize: function() {
+    this.render();
+    this.collection.on('add remove change:playCount', function(){
+      this.render();
+    }.bind(this));
   },
 
-  render: function() {
-    return this.$el;
-  }
+  render: function(){
+    // to preserve event handlers on child nodes, we must call .detach() on them before overwriting with .html()
+    // see http://api.jquery.com/detach/
+    this.$el.children().detach();
 
+    this.$el.html('<th>Queue</th>').append(
+      this.collection.map(function(song){
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
+  }
 });
